f2d5787842ad900eed329840c3f0f026ed887037
sk <sarandhanush123@gmail.com>
2020-06-20 19:56:56 +0530

    init commit

---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,19 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<agora_rtm/AgoraRtmPlugin.h>)
+#import <agora_rtm/AgoraRtmPlugin.h>
+#else
+@import agora_rtm;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [AgoraRtmPlugin registerWithRegistrar:[registry registrarForPlugin:@"AgoraRtmPlugin"]];
+}
+
+@end
